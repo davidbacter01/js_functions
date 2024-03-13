186f6fd61a321f099c4818a93cf678b7858e8329
davidbacter01 <davidbacter01@gmail.com>
2024-03-13 19:10:52 +0200

    Add example for a function that checks if an array has an element

--- script.js
+++ script.js
@@ -9,3 +9,31 @@ function printDigits() {
 printDigits();
 printDigits();
 printDigits();
+
+
+// functii cu parametri
+function arrayHasElement(arrayToCheck, element) {
+    let found = false;
+    for (const el of arrayToCheck) {
+        if (el === element) {
+            found = true;
+            break;
+        }
+    }
+
+    if (found) {
+        console.log(`Aray ${arrayToCheck} has element ${element}`);
+    } else {
+        console.log(`Aray ${arrayToCheck} does not have element ${element}`);
+    }
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 10, 21];
+const valueToCheck1 = 10;
+const valueToCheck2 = 30;
+
+arrayHasElement(array1, valueToCheck1);
+arrayHasElement(array2, valueToCheck1);
+arrayHasElement(array1, valueToCheck2);
+arrayHasElement(['ala', 'bala', 'portocala'], 'ala');
