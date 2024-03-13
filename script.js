20fdf7e00d2f26b54868aa97e64021489af6aea7
davidbacter01 <davidbacter01@gmail.com>
2024-03-13 18:54:07 +0200

    Add example of a simple function that prints digits

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello');
+// fuctii simple
+
+function printDigits() {
+    for (let i = 0; i <= 9; i++) {
+        console.log('digit: ', i);
+    }
+}
+
+printDigits();
+printDigits();
+printDigits();
